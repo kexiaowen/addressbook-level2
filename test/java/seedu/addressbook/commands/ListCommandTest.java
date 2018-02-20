4b5ed15791c814575ff0564ee553a450a163d78e
Ke Xiaowen <e0032602@u.nus.edu>
2018-02-20 23:33:01 +0800

    Add JUnit test for list command

---
+++ test/java/seedu/addressbook/commands/ListCommandTest.java
@@ -0,0 +1,38 @@
+package seedu.addressbook.commands;
+
+import org.junit.Test;
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.util.TypicalPersons;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class ListCommandTest {
+
+    private final AddressBook addressBook = new TypicalPersons().getTypicalAddressBook();
+    private final TypicalPersons td = new TypicalPersons();
+
+    @Test
+    public void execute() {
+        assertListCommandBehavior(Arrays.asList(td.amy, td.bill, td.candy, td.dan));
+    }
+
+    /**
+     * Executes the list command and verifies the result matches
+     * the persons in the expectedPersonList exactly.
+     */
+    private void assertListCommandBehavior(List<ReadOnlyPerson> expectedPersonList) {
+        ListCommand command = createListCommand();
+        CommandResult result = command.execute();
+
+        assertEquals(Command.getMessageForPersonListShownSummary(expectedPersonList), result.feedbackToUser);
+    }
+
+    private ListCommand createListCommand() {
+        ListCommand command = new ListCommand();
+        command.setData(addressBook, Collections.emptyList());
+        return command;
+    }
+}
